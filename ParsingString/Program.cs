0f3736967279d433a77861486b1b4a42e1b7f097
yichunzhao <zhaoyichun@gmail.com>
2022-04-14 22:16:49 +0200

    Initial commit

---
+++ ParsingString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ParsingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing double string");
+            var target = "0.2";
+            Console.WriteLine("Parsing " + target);
+
+            double result = Double.Parse(target);
+            CheckResult(result);
+
+            var _target = "0,2";
+            Console.WriteLine("Parsing " + _target);
+            double _result = Double.Parse(_target);
+            CheckResult(_result);
+        }
+
+        private static void CheckResult(double result)
+        {
+            Console.WriteLine(result);
+            
+            if (result == 0.2)
+            {
+                Console.WriteLine("result is 0.2");
+            }
+
+            if (result == 2)
+            {
+                Console.WriteLine("result is 2");
+            }
+        }
+    }
+}
